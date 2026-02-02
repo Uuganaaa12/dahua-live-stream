a5cf444da818a38954d54e697f37a635d4e3146b
b232270004u <uugnaauugaa56@gmail.com>
2026-02-02 10:40:09 +0800

    v3

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,36 +1,39 @@
 import { Component, computed, signal } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
+interface Camera {
+  id: number;
+  name: string;
+  ip: string;
+}
+
 @Component({
   selector: 'app-root',
-  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly localIp = signal('');
 
-  private readonly streamUrlRaw = computed(() => {
-    const ip = this.localIp().trim();
-    if (!ip) {
-      return null;
-    }
+  protected readonly cameras = signal<Camera[]>([
+    { id: 1, name: 'Camera 1', ip: '192.168.1.201' },
+    { id: 2, name: 'Camera 2', ip: '192.168.1.202' },
+    { id: 3, name: 'Camera 3', ip: '192.168.1.203' },
+  ]);
 
-    return `http://${ip}/cgi-bin/mjpg/video.cgi?channel=1&subtype=0`;
-  });
-
-  protected readonly streamUrl = computed<SafeResourceUrl | null>(() => {
-    const url = this.streamUrlRaw();
-    if (!url) {
-      return null;
-    }
-
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
-  });
+  protected readonly selectedCamera = signal<Camera | null>(null);
 
   constructor(private readonly sanitizer: DomSanitizer) {}
 
-  protected onIpChange(value: string): void {
-    this.localIp.set(value.trim());
+  protected getStreamUrl(ip: string): SafeResourceUrl {
+    const url = `http://${ip}/cgi-bin/mjpg/video.cgi?channel=1&subtype=0`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  protected open(camera: Camera) {
+    this.selectedCamera.set(camera);
+  }
+
+  protected close() {
+    this.selectedCamera.set(null);
   }
 }
