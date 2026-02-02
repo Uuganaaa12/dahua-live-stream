6082ff7e0aa4bff4b209a69a60fe7c9703946316
b232270004u <uugnaauugaa56@gmail.com>
2026-02-02 10:44:37 +0800

    v4

--- src/app/app.ts
+++ src/app/app.ts
@@ -15,9 +15,9 @@ interface Camera {
 export class App {
 
   protected readonly cameras = signal<Camera[]>([
-    { id: 1, name: 'Camera 1', ip: '192.168.1.201' },
-    { id: 2, name: 'Camera 2', ip: '192.168.1.202' },
-    { id: 3, name: 'Camera 3', ip: '192.168.1.203' },
+    { id: 1, name: 'Camera 1', ip: '192.168.1.200' },
+    { id: 2, name: 'Camera 2', ip: '192.168.1.201' },
+    { id: 3, name: 'Camera 3', ip: '192.168.1.202' },
   ]);
 
   protected readonly selectedCamera = signal<Camera | null>(null);
